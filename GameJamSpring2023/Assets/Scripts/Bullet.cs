8bb2c3040cacc08412aaa50cc02b118df51a9f70
anoble <alixkitty@gmail.com>
2023-03-25 17:07:34 -0500

    BULLETS!!! no poopy :)

---
+++ GameJamSpring2023/Assets/Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] int speed;
+
+    public int bulletSpeed()
+    {
+        return speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.tag.Equals("Player")) {
+            Debug.Log("hit detected");
+            Destroy(gameObject);
+        }
+
+    }
+}
