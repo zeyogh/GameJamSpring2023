f8d445998666a5e843342765994ecb3bccd470a7
egasner <egasner68@gmail.com>
2023-03-25 14:03:31 -0500

    Some Space Station Jazz

--- GameJamSpring2023/Assets/Scripts/SpaceStationScript.cs
+++ GameJamSpring2023/Assets/Scripts/SpaceStationScript.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class SpaceStationScript : MonoBehaviour
 {
-    int OnOrOff = 0; // on is 1 off is 0
+    public int OnOrOff = 0; // on is 1 off is 0
+    int playerIsInMeXD = 0;
 
-    void OnTriggerStay2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (other.tag == "Player")
         {
-            OnOrOff = 1;
-            Debug.Log("Station Has been interacted with: " + OnOrOff);
+            playerIsInMeXD = 1;
+            Debug.Log("In SpaceStation");
             
         }
-        Debug.Log("In SpaceStation");
     }
+
+    void OnTriggerExit2D(Collider2D other){
+        if (other.tag == "Player")
+        {
+            playerIsInMeXD = 0;
+            Debug.Log("NOT In SpaceStation");
+            
+        }
+    }
+
+    void FixedUpdate(){
+        if(playerIsInMeXD == 1 && Input.GetKeyDown(KeyCode.Space)){
+            Debug.Log("Space Station is ACTIVE");
+        }
+    }
+    
 }
