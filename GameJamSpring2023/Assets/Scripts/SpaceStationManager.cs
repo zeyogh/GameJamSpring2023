352ede73f890f28c210854d94d029707e57af7a0
egasner <egasner68@gmail.com>
2023-03-25 14:35:59 -0500

    space Station and beam Prefab

--- GameJamSpring2023/Assets/Scripts/SpaceStationManager.cs
+++ GameJamSpring2023/Assets/Scripts/SpaceStationManager.cs
@@ -16,9 +16,13 @@ public class SpaceStationManager : MonoBehaviour
     }
 
     void Update(){
+
+        // Debug.Log(SS1.GetComponent<SpaceStationScript>().OnOrOff);
+        // Debug.Log(SS2.GetComponent<SpaceStationScript>().OnOrOff);
         if(SS1.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS2.GetComponent<SpaceStationScript>().OnOrOff == 1 && beamSpawned == 0){
             beamSpawned = 1;
             Debug.Log("Beam Should Spawn and be visible");
+            
         }
 
         
