f158eae142f87704f8e5250db090df5cd78dca7a
egasner <egasner68@gmail.com>
2023-03-25 22:27:05 -0500

    space shit stuff

--- GameJamSpring2023/Assets/Scripts/SpaceStationManager.cs
+++ GameJamSpring2023/Assets/Scripts/SpaceStationManager.cs
@@ -7,14 +7,20 @@ public class SpaceStationManager : MonoBehaviour
     [SerializeField]GameObject SS1;
     [SerializeField]GameObject SS2;
     [SerializeField]GameObject SS3;
+    [SerializeField]GameObject SS4;
+    [SerializeField]GameObject SS5;
+    [SerializeField]GameObject SS6;
+    [SerializeField]GameObject SS7;
+    [SerializeField]GameObject SS8;
+    [SerializeField]GameObject SS9;
     [SerializeField]GameObject beamPrefab;
-    GameObject[] SpSt = new GameObject[3];
+    GameObject[] SpSt = new GameObject[10];
     int index = 0;
     float x, y, x2, y2;
     
     GameObject SpawnedBeam;
-    int beamSpawned0 = 0;
-    int beamSpawned1 = 0;
+    int[] beamSpawned = new int[9];
+    
 
     
     
@@ -29,7 +35,7 @@ public class SpaceStationManager : MonoBehaviour
     }
 
     void Update(){
-
+        
         if(SS1.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS1.GetComponent<SpaceStationScript>().arrAdded == 0){
             SpSt[index] = SS1;
             SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
@@ -45,19 +51,60 @@ public class SpaceStationManager : MonoBehaviour
             SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
             index++;
         }
+        if(SS4.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS4.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS4;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+        if(SS5.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS5.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS5;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+        if(SS6.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS6.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS6;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+        if(SS7.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS7.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS7;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+        if(SS8.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS8.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS8;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+        if(SS9.GetComponent<SpaceStationScript>().OnOrOff == 1 && SS9.GetComponent<SpaceStationScript>().arrAdded == 0){
+            SpSt[index] = SS9;
+            SpSt[index].GetComponent<SpaceStationScript>().arrAdded = 1;
+            index++;
+        }
+
+        if(index == 9){
+            SpSt[0].GetComponent<SpaceStationScript>().arrAdded = 0;
+            SpSt[0].GetComponent<SpaceStationScript>().OnOrOff = 0;
+        }
 
         
-        if(index==2 && beamSpawned0 == 0){
+        
+        if(index==2 && beamSpawned[index-2] == 0){
             float angleRotaion;
             float diagDistance;
 
-            beamSpawned0 = 1;
-            x = SpSt[0].transform.position.x;
-            x2 = SpSt[1].transform.position.x;
-            y = SpSt[0].transform.position.y;
-            y2 = SpSt[1].transform.position.y;
+            beamSpawned[index-2] = 1;
+            x = SpSt[index-2].transform.position.x;
+            x2 = SpSt[index-1].transform.position.x;
+            y = SpSt[index-2].transform.position.y;
+            y2 = SpSt[index-1].transform.position.y;
 
-            angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
             diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
 
             SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
@@ -68,17 +115,197 @@ public class SpaceStationManager : MonoBehaviour
             
         }
 
-        if(index==3 && beamSpawned1 == 0){
+        if(index==3 && beamSpawned[index-2] == 0){
             float angleRotaion;
             float diagDistance;
 
-            beamSpawned1 = 1;
-            x = SpSt[2].transform.position.x;
-            x2 = SpSt[1].transform.position.x;
-            y = SpSt[2].transform.position.y;
-            y2 = SpSt[1].transform.position.y;
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
 
-            angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==4 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==5 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==6 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==7 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==8 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==9 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
+            diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
+
+            SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
+
+            SpawnedBeam.transform.localScale = new Vector3(diagDistance/12,1,1);
+
+            Debug.Log("Beam Should Spawn and be visible");
+            
+        }
+        if(index==10 && beamSpawned[index-2] == 0){
+            float angleRotaion;
+            float diagDistance;
+
+            beamSpawned[index-2]= 1;
+            x = SpSt[index-1].transform.position.x;
+            x2 = SpSt[index-2].transform.position.x;
+            y = SpSt[index-1].transform.position.y;
+            y2 = SpSt[index-2].transform.position.y;
+
+            if((x-x2)*(x-x2) > ((y-y2)*(y-y2))){    
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).5;
+            }else
+            {
+                angleRotaion = (float)System.Math.Atan((y-y2)/(x-x2))*(float).605;
+            }
             diagDistance = Mathf.Sqrt(((x-x2)*(x-x2)) + ((y-y2)*(y-y2)));
 
             SpawnedBeam = Instantiate(beamPrefab, new Vector3(((x+x2)/2), ((y+y2)/2), 0), new Quaternion(0,0,angleRotaion,1));
