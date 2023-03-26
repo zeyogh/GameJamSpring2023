cd331e1bbf2cc5e8910108808a5a5891c6c40ae9
egasner <egasner68@gmail.com>
2023-03-26 10:43:55 -0500

    alotta garbage stuff

--- GameJamSpring2023/Assets/Scripts/SpaceStationScript.cs
+++ GameJamSpring2023/Assets/Scripts/SpaceStationScript.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class SpaceStationScript : MonoBehaviour
 {
+    public Sprite TurnedOn;
     public int OnOrOff = 0; // on is 1 off is 0
     int playerIsInMeXD = 0;
     public int arrAdded = 0;
@@ -32,11 +35,15 @@ public class SpaceStationScript : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.M)){
              if(playerIsInMeXD == 1 && OnOrOff == 0){
                 OnOrOff = 1;
-                Debug.Log("Daddy Im on!!!");
+                TurnOn();
              }
         }
 
         
+    }
+
+    void TurnOn(){
+        this.GetComponent<SpriteRenderer>().sprite = TurnedOn;
     }
     
 }
